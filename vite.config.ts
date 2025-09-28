20941544dd026cae75012db9feb554db5428e617
thunder-svg <mfleon32@gmail.com>
2025-09-28 17:37:00 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig } from "vite";
-
 export default defineConfig({
-  root: "src",                // 엔트리를 src로
-  base: "/cjsend/",           // GH Pages 하위 경로
-  publicDir: "../public",     // public은 저장소 루트에 유지
-  build: { outDir: "../docs", emptyOutDir: true } // 산출물은 docs/
+  root: "src",
+  base: "/cjsend/",
+  publicDir: "../public",
+  build: { outDir: "../docs", emptyOutDir: true }
 });
